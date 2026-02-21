58c0051a7d71d93df7edacce67a07055dfe7678e
Bam <piyaon-p@hotmail.com>
2026-02-21 23:52:14 +0700

    add more locator method

--- tests/practiceLocator.spec.ts
+++ tests/practiceLocator.spec.ts
@@ -4,7 +4,9 @@ test("Locator", async({page}) =>{
 
     await page.goto("https://www.saucedemo.com");
     await page.locator("//*[@name='user-name']").fill("standard_user"); //XPath locater //*[@attributes='attributeValue]
-
-
+    // use * instead of htmltag (like inout) to avoid when dev is changing the htmltag often so the code still good to go
+    await page.locator('input#password').fill('secret_sauce'); //CSS Selectors htmltag#idValue
+    await page.locator(`.submit-button`).click(); // CSS Selectors .classValue (name: in html)
+    await page.locator("text='Sauce Labs Backpack'").click() //Text with ' ' is Case Sensitive but without ' ' is Non Case Sensitive
 
 })
